46bf4f3a8c57a6ec9b12458172c30dabcf761860
pravein <praveingk@gmail.com>
2017-03-06 11:56:11 +0800

    Scripts

---
+++ scripts/bnv.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+OVXHOME=`dirname $0`/..
+OVX_JAR="${OVXHOME}/target/OpenVirteX.jar"
+
+
+JVM_OPTS="-Xms512m -Xmx2g"
+## If you want JaCoCo Code Coverage reports... uncomment line below
+#JVM_OPTS="$JVM_OPTS -javaagent:${OVXHOME}/lib/jacocoagent.jar=dumponexit=true,output=file,destfile=${OVXHOME}/target/jacoco.exec"
+JVM_OPTS="$JVM_OPTS -XX:+TieredCompilation"
+JVM_OPTS="$JVM_OPTS -XX:+UseCompressedOops"
+JVM_OPTS="$JVM_OPTS -XX:+UseConcMarkSweepGC -XX:+AggressiveOpts -XX:+UseFastAccessorMethods"
+JVM_OPTS="$JVM_OPTS -XX:MaxInlineSize=8192 -XX:FreqInlineSize=8192" 
+JVM_OPTS="$JVM_OPTS -XX:CompileThreshold=1500 -XX:PreBlockSpin=8" 
+
+if [ ! -e ${OVX_JAR} ]; then
+  cd ${OVXHOME}
+  echo "Packaging BNV for you..."
+  mvn package 
+  cd -
+fi
+
+echo "Starting BNV..."
+java ${JVM_OPTS} -Dlog4j.configurationFile=${OVXHOME}/config/log4j2.xml -Djavax.net.ssl.keyStore=${OVXHOME}/config/sslStore -jar ${OVX_JAR} $@
